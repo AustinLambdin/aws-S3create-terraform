4d09e35b8ff5851ac3925009d57ef6fb73b2a358
Austin Lambdin <austin.lambdin@gmail.com>
2024-06-27 17:38:47 -0700

    Change main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,10 @@
 provider "aws" {
-  region = "us-west-2" # Choose your desired AWS region
+  region = "us-east-2"
+  assume_role {
+    role_arn = "arn:aws:iam::029096208579:user/Austin"
+  }
 }
+
 module "s3-bucket" {
   source  = "terraform-aws-modules/s3-bucket/aws"
   version = "4.1.2"
